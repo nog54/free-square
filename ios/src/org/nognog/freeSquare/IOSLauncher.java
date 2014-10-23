dc10cab686dc388ab95ba31d58b3dc8df7d77a3d
Goshi Noguchi <noggon54@gmail.com>
2014-10-23 10:07:19 +0900

    Initialize commit.

---
+++ ios/src/org/nognog/freeSquare/IOSLauncher.java
@@ -0,0 +1,22 @@
+package org.nognog.freeSquare;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import org.nognog.freeSquare.FreeSquare;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new FreeSquare(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
