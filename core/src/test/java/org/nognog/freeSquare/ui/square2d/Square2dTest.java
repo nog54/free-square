177850e6c09f5d255f881578baec26c8e04e9947
Goshi Noguchi <noggon54@gmail.com>
2015-01-20 13:40:23 +0900

    Created use gdx tests.

---
+++ core/src/test/java/org/nognog/freeSquare/ui/square2d/Square2dTest.java
@@ -0,0 +1,113 @@
+package org.nognog.freeSquare.ui.square2d;
+
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Test;
+import org.nognog.freeSquare.GdxUseTest;
+import org.nognog.freeSquare.ui.SquareObserver;
+
+public class Square2dTest extends GdxUseTest{
+
+	@Test
+	public final void testAct() {
+		SquareObserver observer = mock(SquareObserver.class);
+		//Square2d suqare = new Square2d(size, baseVertex1, baseVertex2, baseVertex3, baseVertex4, texture)
+	}
+
+	@Test
+	public final void testGetWidth() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetHeight() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSquare2d() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testDrawBatchFloat() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAddSquareObjectSquare2dObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAddSquareObjectSquare2dObjectFloatFloat() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAddActorActor() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetObjects() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testRequestDrawOrderUpdate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetSquareSize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetSquareImage() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testIsConcave() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testContainsInSquareArea() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testDispose() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testIsDisposed() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAddSquareObserver() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testRemoveSquareObserver() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testNotifyObservers() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
