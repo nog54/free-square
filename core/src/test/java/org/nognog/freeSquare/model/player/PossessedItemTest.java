eede2ad8348a6378f724d9944ac94f03e0dd8636
Goshi Noguchi <noggon54@gmail.com>
2015-01-21 23:28:40 +0900

    Create PossessedItemTest.

---
+++ core/src/test/java/org/nognog/freeSquare/model/player/PossessedItemTest.java
@@ -0,0 +1,67 @@
+package org.nognog.freeSquare.model.player;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Test;
+import org.nognog.freeSquare.model.item.Item;
+
+@SuppressWarnings({ "javadoc", "static-method", "rawtypes", "boxing" })
+public class PossessedItemTest {
+
+	@Test
+	public final void testGetItem() {
+		Item mock = mock(Item.class);
+		PossessedItem<?> items = new PossessedItem<>(mock);
+		assertThat(items.getItem(), is(mock));
+	}
+
+	@Test
+	public final void testGetQuantity() {
+		Item mock = mock(Item.class);
+		PossessedItem<?> items = new PossessedItem<>(mock);
+		final int expected1 = 0;
+		final int actual1 = items.getQuantity();
+		assertThat(actual1, is(expected1));
+		
+		items.increaseQuantity(4);
+		
+		final int expected2 = 4;
+		final int actual2 = items.getQuantity();
+		assertThat(actual2, is(expected2));
+		
+		items.increaseQuantity(0);
+		
+		final int expected3 = 4;
+		final int actual3 = items.getQuantity();
+		assertThat(actual3, is(expected3));
+		
+		items.increaseQuantity(-5);
+		
+		final int expected4 = 0;
+		final int actual4 = items.getQuantity();
+		assertThat(actual4, is(expected4));
+		
+		items.increaseQuantity(PossessedItem.maxQuantity * 2);
+		
+		final int expected5 = PossessedItem.maxQuantity;
+		final int actual5 = items.getQuantity();
+		assertThat(actual5, is(expected5));
+		
+		items.decreaseQuantity(-2);
+		
+		final int expected6 = PossessedItem.maxQuantity;
+		final int actual6 = items.getQuantity();
+		assertThat(actual6, is(expected6));
+		
+		items.decreaseQuantity(5);
+		items.decreaseQuantity(0);
+		
+		final int expected7 = PossessedItem.maxQuantity - 5;
+		final int actual7 = items.getQuantity();
+		assertThat(actual7, is(expected7));
+		
+	}
+
+}
